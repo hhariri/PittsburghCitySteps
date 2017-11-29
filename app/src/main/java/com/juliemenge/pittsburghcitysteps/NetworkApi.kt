28a390341368c93858afc3a9de70c2689b779bac
hadihariri <mail@hadihariri.com>
2017-11-29 17:26:43 +0100

    Refactored to use string templates

--- app/src/main/java/com/juliemenge/pittsburghcitysteps/NetworkApi.kt
+++ app/src/main/java/com/juliemenge/pittsburghcitysteps/NetworkApi.kt
@@ -4,10 +4,8 @@ import java.net.URL
 
 class NetworkApi {
     fun getDataFromServer(neighborhood: String): String {
-
-        val incompleteResult = URL("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT * from \"43f40ca4-2211-4a12-8b4f-4d052662bb64\" WHERE neighborhood LIKE ")
-        val result = URL(incompleteResult.toString() + neighborhood).readText()
-
-        return result
+        // Review: Use String Templates as opposed to concatenation
+        val storage = "\"43f40ca4-2211-4a12-8b4f-4d052662bb64\""
+        return URL("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT * from $storage WHERE neighborhood LIKE $neighborhood").readText()
     }
 }
